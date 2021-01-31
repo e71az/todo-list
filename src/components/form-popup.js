1ea6007d9a371cea001ce7b921fcbf19bf999386
Elias Castañeda <e71az@icloud.com>
2021-01-30 23:39:04 -0700

    Created open/close form functionality

--- src/components/form-popup.js
+++ src/components/form-popup.js
@@ -1,16 +1,11 @@
 import { $ } from "./utilities";
 
-// function openForm(event) {
-//   $("#myForm").style.display = "block";
-//   return console.log(event.id);
-// }
-
 function openForm(event) {
   $("#myForm").classList.remove("d-none");
-  // return console.log(event.id);
+  return console.log(event.id);
 }
 
-function closeForm(event) {
+function closeForm() {
   $("#myForm").classList.add("d-none");
   $(".form-container").reset();
   // return console.log(event.id);
