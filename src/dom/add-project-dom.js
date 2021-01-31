88d4c7649858f562482b945e5d3b89a4bc076db6
Elias Castañeda <e71az@icloud.com>
2021-01-31 03:03:00 -0700

    Converted add-project-dom.js to jquery syntax

--- src/dom/add-project-dom.js
+++ src/dom/add-project-dom.js
@@ -1,9 +1,9 @@
-import { $ } from "../components/utilities";
+import $ from "jquery";
 
 const addProjectToDom = (Project = {}) => {
   let newProject = `<li id='project-${Project.id}'>${Project.title}</li>`;
 
-  $(".project-info-list").innerHTML += newProject;
+  $(".project-info-list").append(newProject);
 };
 
 export default addProjectToDom;
