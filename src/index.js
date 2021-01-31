e736bcf39576bc483abf15a41f2f582fe44e6804
Elias Castañeda <e71az@icloud.com>
2021-01-31 00:36:58 -0700

    Changes to get information from project form

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import addProjectToDom from "./dom/add-project-dom";
 import Project from "./components/project";
 import { openForm, closeForm } from "./components/form-popup.js";
+import { $ } from "./components/utilities";
 
 let projectsArray = [new Project("Sample Project")];
 
 projectsArray.forEach((project, index) => addProjectToDom(project, index));
 
+function addProject() {
+  let data = $(".form-add-project").serialize();
+  console.log(data);
+}
+
+window.addProject = addProject;
 window.openForm = openForm;
 window.closeForm = closeForm;
